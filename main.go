f056ca16b2ef2eb67e27e1840acb255492ce4026
Conall <conallimlah@gmail.com>
2021-11-18 20:36:39 +0900

    basic hello world + add .env to gitignore

--- main.go
+++ main.go
@@ -1,7 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	router := mux.NewRouter()
+	
+	router.HandleFunc("/", HealthCheck).Methods("GET")
+	router.HandleFunc("/hello", HelloWorld).Methods("GET")
+
+	http.Handle("/", router)
+	http.ListenAndServe(":8080", router)
 }
+
+func HealthCheck(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+
+		fmt.Fprintf(w, "Server is up and running on port 8080")
+	}
+
+	func HelloWorld(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+
+		w.Write([]byte("Hello World"))
+	}
