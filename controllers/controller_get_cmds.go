d9789f92d6cfb2deedebe0c32be52cc9ea4f4c9d
Conall <conallimlah@gmail.com>
2021-11-21 00:37:18 +0900

    add basic get cmds endpoint

---
+++ controllers/controller_get_cmds.go
@@ -0,0 +1,27 @@
+package controllers
+
+import (
+	"fmt"
+
+	"github.com/conalli/bookshelf-backend/db"
+	"github.com/conalli/bookshelf-backend/models"
+	"github.com/conalli/bookshelf-backend/utils/password"
+)
+
+func GetAllCmds(requestData models.GetCmdsReq) (map[string]string, error) {
+	ctx, cancelFunc := db.MongoContext()
+	client := db.MongoClient(ctx)
+	defer cancelFunc()
+	defer client.Disconnect(ctx)
+
+	collection := db.MongoCollection(client, "users")
+	user, err := models.GetUserByName(ctx, &collection, requestData.Name)
+	if err != nil {
+		return nil, err
+	}
+	correctPassword := password.CheckHashedPassword(user.Password, requestData.Password)
+	if !correctPassword {
+		return nil, fmt.Errorf("could not return commands, incorrect password")
+	}
+	return user.Bookmarks, nil
+}
--- routes/route_get_cmds.go
+++ routes/route_get_cmds.go
@@ -1,7 +1,31 @@
 package routes
 
-import "net/http"
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	"github.com/conalli/bookshelf-backend/controllers"
+	"github.com/conalli/bookshelf-backend/models"
+)
 
 func GetCmds(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello from getcmds"))
+	fmt.Println("GetCmds endpoint hit")
+	var getCmdsReq models.GetCmdsReq
+	json.NewDecoder(r.Body).Decode(&getCmdsReq)
+
+	cmds, err := controllers.GetAllCmds(getCmdsReq)
+	if err != nil {
+		fmt.Printf("error returned while trying to create a new user: %v", err)
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusBadRequest)
+		var testError tempError
+		testError.Error = err.Error()
+		json.NewEncoder(w).Encode(testError)
+	} else {
+		fmt.Printf("successfully retrieved cmds: %v", cmds)
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		json.NewEncoder(w).Encode(cmds)
+	}
 }
