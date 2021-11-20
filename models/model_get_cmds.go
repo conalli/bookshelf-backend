fcf20c49a59ca2b2f830b2b8ab72dc9e1b4ea1a8
Conall <conallimlah@gmail.com>
2021-11-21 00:46:56 +0900

    move GetUserByName to general file

--- models/model_get_cmds.go
+++ models/model_get_cmds.go
@@ -1,27 +1,6 @@
 package models
 
-import (
-	"context"
-
-	"go.mongodb.org/mongo-driver/bson"
-	"go.mongodb.org/mongo-driver/bson/primitive"
-	"go.mongodb.org/mongo-driver/mongo"
-)
-
 type GetCmdsReq struct {
 	Name     string `json:"name"`
 	Password string `json:"password"`
-}
-
-func GetUserByName(ctx context.Context, collection *mongo.Collection, reqName string) (UserData, error) {
-	var result UserData
-	err := collection.FindOne(ctx, bson.D{primitive.E{Key: "name", Value: reqName}}).Decode(&result)
-	if err != nil {
-		if err == mongo.ErrNoDocuments {
-			return result, mongo.ErrNoDocuments
-		} else {
-			return result, err
-		}
-	}
-	return result, nil
 }
--- models/models.go
+++ models/models.go
@@ -1,8 +1,29 @@
 package models
 
+import (
+	"context"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
 type UserData struct {
 	Name      string            `json:"name" bson:"name"`
 	Password  string            `json:"password" bson:"password"`
 	ApiKey    string            `json:"apiKey" bson:"apiKey"`
 	Bookmarks map[string]string `json:"bookmarks" bson:"bookmarks"`
+}
+
+func GetUserByName(ctx context.Context, collection *mongo.Collection, reqName string) (UserData, error) {
+	var result UserData
+	err := collection.FindOne(ctx, bson.D{primitive.E{Key: "name", Value: reqName}}).Decode(&result)
+	if err != nil {
+		if err == mongo.ErrNoDocuments {
+			return result, mongo.ErrNoDocuments
+		} else {
+			return result, err
+		}
+	}
+	return result, nil
 }
