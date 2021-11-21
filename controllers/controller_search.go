868ef55846df592d656b53c229fa1e4fa8695cad
Conall <conallimlah@gmail.com>
2021-11-21 23:02:30 +0900

    add basic search endpoint

---
+++ controllers/controller_search.go
@@ -0,0 +1,29 @@
+package controllers
+
+import (
+	"fmt"
+
+	"github.com/conalli/bookshelf-backend/db"
+	"github.com/conalli/bookshelf-backend/models"
+)
+
+func GetURL(apiKey, cmd string) (string, error) {
+	ctx, cancelFunc := db.MongoContext()
+	client := db.MongoClient(ctx)
+	defer cancelFunc()
+	defer client.Disconnect(ctx)
+
+	collection := db.MongoCollection(client, "users")
+	user, err := models.GetUserByKey(ctx, &collection, "apiKey", apiKey)
+	var defaultSearch = fmt.Sprintf("http://www.google.com/search?q=%s", cmd)
+	fmt.Println(cmd)
+	if err != nil {
+		return defaultSearch, err
+	}
+	url, found := user.Bookmarks[cmd]
+	if !found {
+		return defaultSearch, fmt.Errorf("error: user cmd %s does not exist", cmd)
+	}
+	// TODO: improve url format handling
+	return "http://" + url, nil
+}
--- routes/route_search.go
+++ routes/route_search.go
@@ -1,7 +1,22 @@
 package routes
 
-import "net/http"
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/conalli/bookshelf-backend/controllers"
+	"github.com/gorilla/mux"
+)
 
 func Search(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello from search"))
+	fmt.Println("Search endpoint hit")
+	vars := mux.Vars(r)
+	apiKey := vars["apiKey"]
+	cmd := vars["cmd"]
+	url, err := controllers.GetURL(apiKey, cmd)
+	if err != nil {
+		log.Println(err)
+	}
+	http.Redirect(w, r, url, http.StatusTemporaryRedirect)
 }
