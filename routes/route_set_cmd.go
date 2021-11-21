6cfa477974f90cfabeb2fddd048df15014609551
Conall <conallimlah@gmail.com>
2021-11-21 18:17:43 +0900

    add set cmd route

--- routes/route_set_cmd.go
+++ routes/route_set_cmd.go
@@ -1,7 +1,34 @@
 package routes
 
-import "net/http"
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	"github.com/conalli/bookshelf-backend/controllers"
+	"github.com/conalli/bookshelf-backend/models"
+)
 
 func SetCmd(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello from setcmd"))
+	fmt.Println("SetCmd endpoint hit")
+	var getCmdsReq models.SetCmdReq
+	// Add error handling
+	json.NewDecoder(r.Body).Decode(&getCmdsReq)
+	numUpdated, err := controllers.AddCmd(getCmdsReq)
+	if err != nil || numUpdated == 0 {
+		fmt.Printf("error returned while trying to add a new cmd: %v", err)
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusBadRequest)
+		var testError tempError
+		testError.Error = err.Error()
+		json.NewEncoder(w).Encode(testError)
+	} else {
+		fmt.Printf("successfully set cmd: %s, url: %s", getCmdsReq.Cmd, getCmdsReq.URL)
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		res := models.SetCmdRes{
+			CmdsSet: numUpdated,
+		}
+		json.NewEncoder(w).Encode(res)
+	}
 }
