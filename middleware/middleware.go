3ccfdb14dc20ba765a5a22bc02d2fe76a2d5efdb
Conall <conallimlah@gmail.com>
2021-11-29 23:56:20 +0900

    allow credentials in cors middleware

--- middleware/middleware.go
+++ middleware/middleware.go
@@ -10,9 +10,10 @@ import (
 
 // CORSMiddleware sets CORS options for the main router.
 func CORSMiddleware(r *mux.Router) http.Handler {
-	allowedURL := []string{os.Getenv("ALLOWED_URL")}
+	allowedURL := []string{os.Getenv("ALLOWED_URL_BASE"), os.Getenv("ALLOWED_URL_DASHBOARD")}
 	methods := handlers.AllowedMethods([]string{"GET", "POST", "PUT"})
 	origins := handlers.AllowedOrigins(allowedURL)
+	credentials := handlers.AllowCredentials()
 	headers := handlers.AllowedHeaders([]string{"Content-Type"})
-	return handlers.CORS(methods, origins, headers)(r)
+	return handlers.CORS(methods, origins, credentials, headers)(r)
 }
