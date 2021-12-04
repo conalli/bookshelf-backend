db12d356fc04f7cce951680f1091726a369c8e7f
Conall <conallimlah@gmail.com>
2021-12-04 22:39:52 +0900

    add models for del cmd

---
+++ models/model_del_cmd.go
@@ -0,0 +1,36 @@
+package models
+
+import (
+	"context"
+	"fmt"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+// DelCmdReq represents the expected fields needed for the delcmd request to be completed.
+type DelCmdReq struct {
+	Name     string `json:"name" bson:"name"`
+	Password string `json:"password" bson:"password"`
+	Cmd      string `json:"cmd" bson:"cmd"`
+}
+
+// DelCmdRes represents the fields returned by the delcmd endpoint.
+type DelCmdRes struct {
+	NumDeleted int    `json:"numDeleted"`
+	Cmd        string `json:"cmd"`
+}
+
+// RemoveCmdFromUser takes a given username along with the cmd and removes the cmd from their bookmarks.
+func RemoveCmdFromUser(ctx context.Context, collection *mongo.Collection, username, cmd string) (*mongo.UpdateResult, error) {
+	opts := options.Update().SetUpsert(true)
+	filter := bson.D{primitive.E{Key: "name", Value: username}}
+	update := bson.D{primitive.E{Key: "$unset", Value: bson.D{primitive.E{Key: fmt.Sprintf("bookmarks.%s", cmd), Value: ""}}}}
+	result, err := collection.UpdateOne(ctx, filter, update, opts)
+	if err != nil {
+		return nil, err
+	}
+	return result, nil
+}
