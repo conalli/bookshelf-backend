979fc7951b1a95ae9fadd5a2ca065044e5b97214
Conall <conallimlah@gmail.com>
2021-12-06 17:42:52 +0900

    use redis in search endpoint

--- controllers/controller_search.go
+++ controllers/controller_search.go
@@ -12,20 +12,29 @@ import (
 // or a google search url for the cmd based on whether the cmd could be found or not.
 func GetURL(apiKey, cmd string) (string, apiErrors.ApiErr) {
 	ctx, cancelFunc := db.MongoContext()
-	client := db.MongoClient(ctx)
 	defer cancelFunc()
-	defer client.Disconnect(ctx)
 
-	collection := db.MongoCollection(client, "users")
-	user, err := models.GetUserByKey(ctx, &collection, "apiKey", apiKey)
-	var defaultSearch = fmt.Sprintf("http://www.google.com/search?q=%s", cmd)
+	cache := db.NewRedisClient()
+	url, err := cache.GetSearchData(ctx, apiKey, cmd)
 	if err != nil {
-		return defaultSearch, apiErrors.ParseGetUserError(apiKey, err)
+		client := db.MongoClient(ctx)
+		defer client.Disconnect(ctx)
+		collection := db.MongoCollection(client, "users")
+
+		var user models.UserData
+		user, err = models.GetUserByKey(ctx, &collection, "apiKey", apiKey)
+		var defaultSearch = fmt.Sprintf("http://www.google.com/search?q=%s", cmd)
+		if err != nil {
+			return defaultSearch, apiErrors.ParseGetUserError(apiKey, err)
+		}
+
+		cache.SetSearchData(ctx, apiKey, user.Bookmarks)
+
+		url, found := user.Bookmarks[cmd]
+		if !found {
+			return defaultSearch, apiErrors.NewBadRequestError("error: command: " + cmd + " not registered")
+		}
+		return models.FormatURL(url), nil
 	}
-	url, found := user.Bookmarks[cmd]
-	if !found {
-		return defaultSearch, apiErrors.NewBadRequestError("error: command: " + cmd + " not registered")
-	}
-	url = models.FormatURL(url)
-	return url, nil
+	return models.FormatURL(url), nil
 }
