5090b4e3c96961e5864f4f7a4cffd893aa391acd
Conall <conallimlah@gmail.com>
2021-12-16 19:39:25 +0900

    add initial log for getcmds endpoint

--- routes/route_get_cmds.go
+++ routes/route_get_cmds.go
@@ -13,9 +13,9 @@ import (
 // GetCmds is the handler for the getcmds endpoint. Checks credentials + JWT and if
 // authorized returns all users cmds.
 func GetCmds(w http.ResponseWriter, r *http.Request) {
+	log.Println("getcmds endpoint hit")
 	vars := mux.Vars(r)
 	user := vars["apiKey"]
-
 	cmds, err := controllers.GetAllCmds(r.Context(), user)
 	if err != nil {
 		log.Printf("error returned while trying to get cmds: %v", err)
