5b2b1baaac7e2d34bbd373e930f24b1397ab5c57
Conall <conallimlah@gmail.com>
2022-01-12 23:10:41 +0900

    add CreateTeam controller

--- controllers/controller_sign_up.go
+++ controllers/controller_sign_up.go
@@ -53,3 +53,42 @@ func CreateNewUser(reqCtx context.Context, requestData models.Credentials) (stri
 	}
 	return oid.Hex(), apiKey, nil
 }
+
+// CreateNewTeam checks whether a team name alreadys exists in the db. If not, a new team
+// is created based upon the request data.
+func CreateNewTeam(reqContext context.Context, requestData models.NewTeamReq) (string, apiErrors.ApiErr) {
+	ctx, cancelFunc := db.ReqContextWithTimeout(reqContext)
+	client := db.NewMongoClient(ctx)
+	defer cancelFunc()
+	defer client.DB.Disconnect(ctx)
+
+	collection := client.MongoCollection("teams")
+	teamExists := models.UserFieldAlreadyExists(ctx, &collection, "name", requestData.Name)
+	if teamExists {
+		log.Println("team already exists")
+		return "", apiErrors.NewBadRequestError(fmt.Sprintf("error creating new user; user with name %v already exists", requestData.Name))
+	}
+	hashedPassword, err := password.HashPassword(requestData.Password)
+	if err != nil {
+		log.Printf("couldnt hash password: %+v\n", err)
+		return "", apiErrors.NewInternalServerError()
+	}
+	newTeamData := models.NewTeamData{
+		Name:      requestData.Name,
+		Password:  hashedPassword,
+		ShortName: requestData.ShortName,
+		Members:   map[string]string{requestData.ID: "admin"},
+		Bookmarks: map[string]string{},
+	}
+	res, err := collection.InsertOne(ctx, newTeamData)
+	if err != nil {
+		log.Printf("couldnt insert team %+v\n", err)
+		return "", apiErrors.NewInternalServerError()
+	}
+	teamOID, ok := res.InsertedID.(primitive.ObjectID)
+	if !ok {
+		log.Println("couldnt convert InsertedID to ObjectID")
+		return "", apiErrors.NewInternalServerError()
+	}
+	return teamOID.Hex(), nil
+}
