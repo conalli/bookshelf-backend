fbfcdb0be230c8bd447173eb89a5aa455857f372
Conall <conallimlah@gmail.com>
2022-01-13 22:30:51 +0900

    add req type and addmember func

--- models/teams_models.go
+++ models/teams_models.go
@@ -1,5 +1,16 @@
 package models
 
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
 // NewTeamData represents the db fields needed to create a new team.
 type NewTeamData struct {
 	Name      string            `json:"name" bson:"name"`
@@ -25,4 +36,32 @@ type NewTeamReq struct {
 	Name      string `json:"name"`
 	Password  string `json:"password"`
 	ShortName string `json:"shortName"`
+}
+
+//AddMemberReq represents the clients request to add a new user.
+type AddMemberReq struct {
+	ID         string `json:"id"`
+	TeamID     string `json:"teamId"`
+	MemberName string `json:"memberName"`
+	Role       string `json:"role"`
+}
+
+// AddMemberToTeam attempts to add a new member to a team.
+func AddMemberToTeam(ctx context.Context, collection *mongo.Collection, teamID, memberID, role string) (bool, error) {
+	opts := options.Update().SetUpsert(true)
+	filter, err := primitive.ObjectIDFromHex(teamID)
+	if err != nil {
+		log.Printf("error getting objectid from hex: %+v\n", err)
+		return false, err
+	}
+	update := bson.D{primitive.E{Key: "$set", Value: bson.D{primitive.E{Key: fmt.Sprintf("members.%s", memberID), Value: role}}}}
+	result, err := collection.UpdateByID(ctx, filter, update, opts)
+	if err != nil {
+		log.Printf("error attempting to add user: %s to team: %s: %+v\n", memberID, teamID, err)
+		return false, err
+	}
+	if result.ModifiedCount == 0 && result.UpsertedCount == 0 {
+		return false, nil
+	}
+	return true, nil
 }
