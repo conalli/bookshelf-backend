28c8cf1f3c18c0181b42a15f711b44104f7ddfdc
Conall <conallimlah@gmail.com>
2022-01-28 18:53:40 +0900

    move login to user package

--- models/user/get.go
+++ models/user/get.go
@@ -5,7 +5,40 @@ import (
 
 	"github.com/conalli/bookshelf-backend/db"
 	"github.com/conalli/bookshelf-backend/models/errors"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
 )
+
+// GetUserByID finds and returns user data based on a the users _id.
+func GetUserByID(ctx context.Context, collection *mongo.Collection, userID string) (UserData, error) {
+	id, err := primitive.ObjectIDFromHex(userID)
+	if err != nil {
+		return UserData{}, err
+	}
+	var result UserData
+	err = collection.FindOne(ctx, bson.M{"_id": id}).Decode(&result)
+	if err != nil {
+		if err == mongo.ErrNoDocuments {
+			return result, mongo.ErrNoDocuments
+		}
+		return result, err
+	}
+	return result, nil
+}
+
+// GetUserByKey finds and returns user data based on a key-value pair.
+func GetUserByKey(ctx context.Context, collection *mongo.Collection, reqKey, reqValue string) (UserData, error) {
+	var result UserData
+	err := collection.FindOne(ctx, bson.D{primitive.E{Key: reqKey, Value: reqValue}}).Decode(&result)
+	if err != nil {
+		if err == mongo.ErrNoDocuments {
+			return result, mongo.ErrNoDocuments
+		}
+		return result, err
+	}
+	return result, nil
+}
 
 // GetAllCmds uses req info to get all users current cmds from the db.
 func GetAllCmds(reqCtx context.Context, apiKey string) (map[string]string, errors.ApiErr) {
--- models/user/user.go
+++ models/user/user.go
@@ -2,10 +2,12 @@ package user
 
 import (
 	"context"
+	"net/http"
 
-	"go.mongodb.org/mongo-driver/bson"
-	"go.mongodb.org/mongo-driver/bson/primitive"
-	"go.mongodb.org/mongo-driver/mongo"
+	"github.com/conalli/bookshelf-backend/auth/password"
+	"github.com/conalli/bookshelf-backend/db"
+	"github.com/conalli/bookshelf-backend/models/errors"
+	"github.com/conalli/bookshelf-backend/models/requests"
 )
 
 // NewUserData represents the document fields created when a user signs up.
@@ -25,32 +27,17 @@ type UserData struct {
 	Bookmarks map[string]string `json:"bookmarks" bson:"bookmarks"`
 }
 
-// GetUserByID finds and returns user data based on a the users _id.
-func GetUserByID(ctx context.Context, collection *mongo.Collection, userID string) (UserData, error) {
-	id, err := primitive.ObjectIDFromHex(userID)
-	if err != nil {
-		return UserData{}, err
-	}
-	var result UserData
-	err = collection.FindOne(ctx, bson.M{"_id": id}).Decode(&result)
-	if err != nil {
-		if err == mongo.ErrNoDocuments {
-			return result, mongo.ErrNoDocuments
-		}
-		return result, err
-	}
-	return result, nil
-}
+// CheckCredentials takes in request data, checks the db and returns the username and apikey is successful.
+func CheckCredentials(reqCtx context.Context, requestData requests.CredentialsRequest) (UserData, errors.ApiErr) {
+	ctx, cancelFunc := db.ReqContextWithTimeout(reqCtx)
+	client := db.NewMongoClient(ctx)
+	defer cancelFunc()
+	defer client.DB.Disconnect(ctx)
 
-// GetUserByKey finds and returns user data based on a key-value pair.
-func GetUserByKey(ctx context.Context, collection *mongo.Collection, reqKey, reqValue string) (UserData, error) {
-	var result UserData
-	err := collection.FindOne(ctx, bson.D{primitive.E{Key: reqKey, Value: reqValue}}).Decode(&result)
-	if err != nil {
-		if err == mongo.ErrNoDocuments {
-			return result, mongo.ErrNoDocuments
-		}
-		return result, err
+	collection := client.MongoCollection("users")
+	currUser, err := GetUserByKey(ctx, &collection, "name", requestData.Name)
+	if err != nil || !password.CheckHashedPassword(currUser.Password, requestData.Password) {
+		return UserData{}, errors.NewApiError(http.StatusUnauthorized, errors.ErrWrongCredentials.Error(), "error: name or password incorrect")
 	}
-	return result, nil
+	return currUser, nil
 }
