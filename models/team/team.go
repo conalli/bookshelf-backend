3a8500f3ed80d44f001a9fd013f965fba77c1d78
Conall <conallimlah@gmail.com>
2022-01-28 23:38:45 +0900

    move AddMember/update to sessions & transactions

--- models/team/team.go
+++ models/team/team.go
@@ -1,16 +1,5 @@
 package team
 
-import (
-	"context"
-	"fmt"
-	"log"
-
-	"go.mongodb.org/mongo-driver/bson"
-	"go.mongodb.org/mongo-driver/bson/primitive"
-	"go.mongodb.org/mongo-driver/mongo"
-	"go.mongodb.org/mongo-driver/mongo/options"
-)
-
 // NewTeamData represents the db fields needed to create a new team.
 type NewTeamData struct {
 	Name      string            `json:"name" bson:"name"`
@@ -28,25 +17,4 @@ type Team struct {
 	ShortName string            `json:"shortName" bson:"shortName"`
 	Members   map[string]string `json:"members" bson:"members"`
 	Bookmarks map[string]string `json:"bookmarks" bson:"bookmarks"`
-}
-
-// AddMemberToTeam attempts to add a new member to a team.
-func AddMemberToTeam(ctx context.Context, collection *mongo.Collection, teamID, memberID, role string) (bool, error) {
-	opts := options.Update().SetUpsert(true)
-	filter, err := primitive.ObjectIDFromHex(teamID)
-	if err != nil {
-		log.Printf("error getting objectid from hex: %+v\n", err)
-		return false, err
-	}
-	update := bson.D{primitive.E{Key: "$set", Value: bson.D{primitive.E{Key: fmt.Sprintf("members.%s", memberID), Value: role}}}}
-	result, err := collection.UpdateByID(ctx, filter, update, opts)
-	if err != nil {
-		log.Printf("error attempting to add user: %s to team: %s: %+v\n", memberID, teamID, err)
-		return false, err
-	}
-	if result.ModifiedCount == 0 && result.UpsertedCount == 0 {
-		log.Printf("error attempting to add user: %s to team: %s, team was not modified\n", memberID, teamID)
-		return false, nil
-	}
-	return true, nil
 }
--- models/team/update.go
+++ models/team/update.go
@@ -2,12 +2,19 @@ package team
 
 import (
 	"context"
+	"fmt"
 	"log"
 
 	"github.com/conalli/bookshelf-backend/db"
 	"github.com/conalli/bookshelf-backend/models/errors"
 	"github.com/conalli/bookshelf-backend/models/requests"
 	"github.com/conalli/bookshelf-backend/models/user"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readconcern"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
 )
 
 // AddMember checks whether a username alreadys exists in the db. If not, a new user
@@ -17,18 +24,67 @@ func AddMember(reqCtx context.Context, requestData requests.AddMemberRequest) (b
 	client := db.NewMongoClient(ctx)
 	defer cancelFunc()
 	defer client.DB.Disconnect(ctx)
-
-	userCollection := client.MongoCollection("users")
-	teamCollection := client.MongoCollection("teams")
-	user, err := user.GetByKey(ctx, &userCollection, "name", requestData.MemberName)
+	opts := options.Session().SetDefaultReadConcern(readconcern.Majority())
+	sess, err := client.DB.StartSession(opts)
+	defer sess.EndSession(ctx)
 	if err != nil {
-		return false, errors.NewBadRequestError("couldnt find user with name " + requestData.MemberName)
-	}
-
-	ok, err := AddMemberToTeam(ctx, &teamCollection, requestData.TeamID, user.ID, requestData.Role)
-	if err != nil {
-		log.Printf("error adding member with name: %s to team with id: %s\n error: %+v\n", requestData.MemberName, requestData.TeamID, err)
+		log.Println("could not start db session")
 		return false, errors.NewInternalServerError()
 	}
-	return ok, nil
+	txnOpts := options.Transaction().SetReadPreference(readpref.PrimaryPreferred())
+	res, err := sess.WithTransaction(ctx, func(sessCtx mongo.SessionContext) (interface{}, error) {
+		userCollection := client.MongoCollection("users")
+		update := user.UpdateEmbedOptions{
+			UserKey:   "name",
+			UserValue: requestData.MemberName,
+			Embedded:  "teams",
+			Key:       requestData.TeamID,
+			Value:     requestData.Role,
+		}
+		user, err := user.UpdateEmbedByField(sessCtx, &userCollection, update)
+		if err != nil {
+			log.Println("couldnt update user with name: " + requestData.MemberName)
+			return false, errors.NewBadRequestError("couldnt update user with name: " + requestData.MemberName)
+		}
+		teamCollection := client.MongoCollection("teams")
+		ok, err := AddMemberToTeam(ctx, &teamCollection, requestData.TeamID, user.ID, requestData.Role)
+		if err != nil {
+			log.Printf("error adding member with name: %s to team with id: %s\n error: %+v\n", requestData.MemberName, requestData.TeamID, err)
+			return false, errors.NewInternalServerError()
+		}
+		return ok, nil
+	}, txnOpts)
+	if err != nil {
+		log.Println("error could not start db transaction")
+		return false, errors.NewInternalServerError()
+	}
+	if v, ok := res.(bool); ok {
+		if !v {
+			return false, nil
+		}
+	} else {
+		return false, nil
+	}
+	return true, nil
+}
+
+// AddMemberToTeam attempts to add a new member to a team.
+func AddMemberToTeam(ctx context.Context, collection *mongo.Collection, teamID, memberID, role string) (bool, error) {
+	opts := options.Update().SetUpsert(true)
+	filter, err := primitive.ObjectIDFromHex(teamID)
+	if err != nil {
+		log.Printf("error getting objectid from hex: %+v\n", err)
+		return false, err
+	}
+	update := bson.D{primitive.E{Key: "$set", Value: bson.D{primitive.E{Key: fmt.Sprintf("members.%s", memberID), Value: role}}}}
+	result, err := collection.UpdateByID(ctx, filter, update, opts)
+	if err != nil {
+		log.Printf("error attempting to add user: %s to team: %s: %+v\n", memberID, teamID, err)
+		return false, err
+	}
+	if result.ModifiedCount == 0 && result.UpsertedCount == 0 {
+		log.Printf("error attempting to add user: %s to team: %s, team was not modified\n", memberID, teamID)
+		return false, nil
+	}
+	return true, nil
 }
