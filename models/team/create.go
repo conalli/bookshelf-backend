11b98dd266ffafd5c6e40cfdb43e7a9a0c72c3bf
Conall <conallimlah@gmail.com>
2022-01-29 22:19:06 +0900

    update create team

--- models/team/create.go
+++ models/team/create.go
@@ -11,6 +11,7 @@ import (
 	"github.com/conalli/bookshelf-backend/models/requests"
 	"github.com/conalli/bookshelf-backend/models/user"
 	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
 )
 
 // New checks whether a team name alreadys exists in the db. If not, a new team
@@ -21,8 +22,8 @@ func New(reqContext context.Context, requestData requests.NewTeamRequest) (strin
 	defer cancelFunc()
 	defer client.DB.Disconnect(ctx)
 
-	collection := client.MongoCollection("teams")
-	teamExists := user.DataAlreadyExists(ctx, &collection, "name", requestData.Name)
+	teamCollection := client.MongoCollection("teams")
+	teamExists := user.DataAlreadyExists(ctx, &teamCollection, "name", requestData.Name)
 	if teamExists {
 		log.Println("team already exists")
 		return "", errors.NewBadRequestError(fmt.Sprintf("error creating new user; user with name %v already exists", requestData.Name))
@@ -39,15 +40,38 @@ func New(reqContext context.Context, requestData requests.NewTeamRequest) (strin
 		Members:   map[string]string{requestData.ID: "admin"},
 		Bookmarks: map[string]string{},
 	}
-	res, err := collection.InsertOne(ctx, newTeamData)
+	res, err := client.SessionWithTransaction(ctx, func(sessCtx mongo.SessionContext) (interface{}, error) {
+		result, err := teamCollection.InsertOne(sessCtx, newTeamData)
+		if err != nil {
+			log.Printf("couldnt insert team %+v\n", err)
+			return "", errors.NewInternalServerError()
+		}
+		teamOID, ok := result.InsertedID.(primitive.ObjectID)
+		if !ok {
+			log.Println("couldnt convert InsertedID to ObjectID")
+			return "", errors.NewInternalServerError()
+		}
+		userCollection := client.MongoCollection("users")
+		opts := user.UpdateEmbedOptions{
+			FilterKey:   "_id",
+			FilterValue: requestData.ID,
+			Embedded:    "teams",
+			Key:         teamOID.Hex(),
+			Value:       "admin",
+		}
+		user, err := user.UpdateEmbedByField(sessCtx, &userCollection, opts)
+		if err != nil {
+			log.Printf("couldnt add team: %s to user: %s", newTeamData.Name, user.Name)
+		}
+		return teamOID.Hex(), nil
+	})
 	if err != nil {
-		log.Printf("couldnt insert team %+v\n", err)
+		log.Println("error during session transaction")
 		return "", errors.NewInternalServerError()
 	}
-	teamOID, ok := res.InsertedID.(primitive.ObjectID)
+	v, ok := res.(string)
 	if !ok {
-		log.Println("couldnt convert InsertedID to ObjectID")
 		return "", errors.NewInternalServerError()
 	}
-	return teamOID.Hex(), nil
+	return v, nil
 }
