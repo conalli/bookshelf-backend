b30235b3fcf917150e900df9c0d3d828b4e261f3
Conall <conallimlah@gmail.com>
2022-02-03 00:15:23 +0900

    rename docker folder, update scripts

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,11 +2,11 @@
 
 if [ $1 = "dev" ]
 then
-  docker-compose -f ./docker/docker-compose.dev.yml -p bookshelf_dev build
+  docker-compose -f ./build/docker-compose.dev.yml -p bookshelf_dev build
   exit 0
 elif [ $1 = "test" ]
 then
-  docker-compose -f ./docker/docker-compose.test.yml -p bookshelf_test build
+  docker-compose -f ./build/docker-compose.test.yml -p bookshelf_test build
   exit 0
 else
   exit 1
--- scripts/up.sh
+++ scripts/up.sh
@@ -2,11 +2,11 @@
 
 if [ $1 = "dev" ]
 then
-  docker-compose -f ./docker/docker-compose.dev.yml -p bookshelf_dev up -d
+  docker-compose -f ./build/docker-compose.dev.yml -p bookshelf_dev up -d
   exit 0
 elif [ $1 = "test" ]
 then
-  docker-compose -f ./docker/docker-compose.test.yml -p bookshelf_test up -d
+  docker-compose -f ./build/docker-compose.test.yml -p bookshelf_test up -d
   exit 0
 else
   exit 1
