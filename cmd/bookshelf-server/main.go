7d14a31aea36b0519dcd23826f6330fc6be52463
Conall <conallimlah@gmail.com>
2022-02-04 19:08:55 +0900

    move main.go, endpoint routing

---
+++ cmd/bookshelf-server/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/conalli/bookshelf-backend/pkg/http/rest"
+	"github.com/joho/godotenv"
+)
+
+func loadEnv(env string) {
+	if env == "production" {
+		return
+	}
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Could not load .env file")
+	}
+}
+
+func main() {
+	loadEnv("development")
+
+	router := rest.RouterWithCORS()
+
+	port := os.Getenv("PORT")
+	log.Println("Server up and running on port: " + port)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), router))
+}
