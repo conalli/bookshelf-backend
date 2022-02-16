ab7470d6fdaaf88fe8df99c7089d19ea102f2b70
Conall <conallimlah@gmail.com>
2022-02-16 19:14:40 +0900

    add const for roles

--- pkg/team/team.go
+++ pkg/team/team.go
@@ -9,3 +9,9 @@ type Team struct {
 	Members   map[string]string `json:"members" bson:"members"`
 	Bookmarks map[string]string `json:"bookmarks" bson:"bookmarks"`
 }
+
+// Represents the current authorization roles for team members.
+const (
+	RoleAdmin = "ADMIN"
+	RoleUser  = "USER"
+)
