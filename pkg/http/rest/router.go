2850495b8755fa7d8d1c81a0de2ecc5a21c2b3b2
Conall <conallimlah@gmail.com>
2022-02-17 21:06:05 +0900

    update routing

--- pkg/http/rest/router.go
+++ pkg/http/rest/router.go
@@ -21,26 +21,29 @@ func Router() *mux.Router {
 	t := team.NewService(mongo)
 
 	router := mux.NewRouter()
-
 	router.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("Hello"))
 	}).Methods("GET")
-	router.HandleFunc("/user/signup", handlers.SignUp(u)).Methods("POST")
-	router.HandleFunc("/user/login", handlers.LogIn(u)).Methods("POST")
-	router.HandleFunc("/user/teams/{APIKey}", jwtauth.Authorized(handlers.GetAllTeams(u))).Methods("GET")
-	router.HandleFunc("/user/cmds/{APIKey}", jwtauth.Authorized(handlers.GetCmds(u))).Methods("GET")
-	router.HandleFunc("/user/addcmd/{APIKey}", jwtauth.Authorized(handlers.AddCmd(u))).Methods("PATCH")
-	router.HandleFunc("/user/delcmd/{APIKey}", jwtauth.Authorized(handlers.DelCmd(u))).Methods("PATCH")
-	router.HandleFunc("/user/del/{APIKey}", jwtauth.Authorized(handlers.DelUser(u))).Methods("DELETE")
 
-	router.HandleFunc("/search/{APIKey}/{cmd}", handlers.Search(s)).Methods("GET")
+	user := router.PathPrefix("/user").Subrouter()
+	user.HandleFunc("/", handlers.SignUp(u)).Methods("POST")
+	user.HandleFunc("/{APIKey}", jwtauth.Authorized(handlers.DelUser(u))).Methods("DELETE")
+	user.HandleFunc("/login", handlers.LogIn(u)).Methods("POST")
+	user.HandleFunc("/teams/{APIKey}", jwtauth.Authorized(handlers.GetAllTeams(u))).Methods("GET")
+	user.HandleFunc("/cmds/{APIKey}", jwtauth.Authorized(handlers.GetCmds(u))).Methods("GET")
+	user.HandleFunc("/addcmd/{APIKey}", jwtauth.Authorized(handlers.AddCmd(u))).Methods("PATCH")
+	user.HandleFunc("/delcmd/{APIKey}", jwtauth.Authorized(handlers.DelCmd(u))).Methods("PATCH")
 
-	router.HandleFunc("/team/{APIKey}", jwtauth.Authorized(handlers.NewTeam(t))).Methods("POST")
-	router.HandleFunc("/team/addmember/{APIKey}", jwtauth.Authorized(handlers.AddMember(t))).Methods("PATCH")
-	router.HandleFunc("/team/delself/{APIKey}", jwtauth.Authorized(handlers.DelSelf(t))).Methods("PATCH")
-	router.HandleFunc("/team/delmember/{APIKey}", jwtauth.Authorized(handlers.DelMember(t))).Methods("PATCH")
-	router.HandleFunc("/team/addcmd/{APIKey}", jwtauth.Authorized(handlers.AddTeamCmd(t))).Methods("PATCH")
-	router.HandleFunc("/team/delcmd/{APIKey}", jwtauth.Authorized(handlers.DelTeamCmd(t))).Methods("PATCH")
+	team := router.PathPrefix("/team").Subrouter()
+	team.HandleFunc("/{APIKey}", jwtauth.Authorized(handlers.NewTeam(t))).Methods("POST")
+	team.HandleFunc("/addmember/{APIKey}", jwtauth.Authorized(handlers.AddMember(t))).Methods("PATCH")
+	team.HandleFunc("/delself/{APIKey}", jwtauth.Authorized(handlers.DelSelf(t))).Methods("PATCH")
+	team.HandleFunc("/delmember/{APIKey}", jwtauth.Authorized(handlers.DelMember(t))).Methods("PATCH")
+	team.HandleFunc("/addcmd/{APIKey}", jwtauth.Authorized(handlers.AddTeamCmd(t))).Methods("PATCH")
+	team.HandleFunc("/delcmd/{APIKey}", jwtauth.Authorized(handlers.DelTeamCmd(t))).Methods("PATCH")
+
+	search := router.PathPrefix("/search").Subrouter()
+	search.HandleFunc("/{APIKey}/{cmd}", handlers.Search(s)).Methods("GET")
 
 	return router
 }
