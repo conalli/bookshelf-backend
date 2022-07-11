4e8b2716c2550c2b21ac7c34f2e7ef5a651cdbe1
Conall <conallimlah@gmail.com>
2022-07-11 16:43:28 +0900

    rename/move handlers

--- pkg/http/rest/handlers/delmember.go
+++ pkg/http/rest/handlers/delmember.go
@@ -45,3 +45,35 @@ package handlers
 // 		return
 // 	}
 // }
+
+// // DelSelf is the handler for the delSelf endpoint. Removes member from team.
+// func DelSelf(t accounts.TeamService) func(w http.ResponseWriter, r *http.Request) {
+// 	return func(w http.ResponseWriter, r *http.Request) {
+// 		log.Println("DelSelf endpoint hit")
+// 		var delSelfReq accounts.DelSelfRequest
+// 		json.NewDecoder(r.Body).Decode(&delSelfReq)
+// 		ok, err := t.DelSelf(r.Context(), delSelfReq)
+// 		if err != nil {
+// 			log.Printf("error returned while trying to delete self from team: %v", err)
+// 			errors.APIErrorResponse(w, err)
+// 			return
+// 		}
+// 		res := delSelfResponse{
+// 			TeamID:            delSelfReq.TeamID,
+// 			NumMembersDeleted: 0,
+// 		}
+// 		if !ok {
+// 			log.Printf("failed to delete self from team: %s\n", delSelfReq.ID)
+// 			w.Header().Set("Content-Type", "application/json")
+// 			w.WriteHeader(http.StatusBadRequest)
+// 			json.NewEncoder(w).Encode(res)
+// 			return
+// 		}
+// 		log.Printf("successfully deleted self from team: %s\n", delSelfReq.ID)
+// 		w.Header().Set("Content-Type", "application/json")
+// 		w.WriteHeader(http.StatusOK)
+// 		res.NumMembersDeleted = 1
+// 		json.NewEncoder(w).Encode(res)
+// 		return
+// 	}
+// }
