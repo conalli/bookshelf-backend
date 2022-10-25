5876a0a73add83d7261c8e478a77d60cdfe96b1d
Conall <conallimlah@gmail.com>
2022-10-25 19:25:40 +0900

    move bookmark type to service

--- pkg/services/accounts/accounts.go
+++ pkg/services/accounts/accounts.go
@@ -14,15 +14,6 @@ type User struct {
 	Teams    map[string]string `json:"teams,omitempty" bson:"teams"`
 }
 
-// Bookmark represents a web bookmark.
-type Bookmark struct {
-	ID     string `json:"id" bson:"_id,omitempty"`
-	APIKey string `json:"APIKey" bson:"APIKey"`
-	Path   string `json:"path" bson:"path"`
-	Name   string `json:"name" bson:"name"`
-	URL    string `json:"url" bson:"url"`
-}
-
 // GenerateAPIKey generates a random URL-safe string of random length for use as an API key.
 func GenerateAPIKey() (string, error) {
 	key, err := uuid.NewRandom()
