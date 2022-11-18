86168a5db1eb7ac5f98a18bece2bb182ae96e34e
Conall <conallimlah@gmail.com>
2022-11-18 13:17:04 +0000

    add basic handler

--- pkg/http/rest/handlers/addbookmarkfile.go
+++ pkg/http/rest/handlers/addbookmarkfile.go
@@ -1,27 +1,39 @@
 package handlers
 
 import (
+	"encoding/json"
 	"net/http"
 
+	"github.com/conalli/bookshelf-backend/pkg/errors"
 	"github.com/conalli/bookshelf-backend/pkg/logs"
 	"github.com/conalli/bookshelf-backend/pkg/services/bookmarks"
+	"github.com/gorilla/mux"
 )
 
-// AddBookmarkFile attempts to add bookmarks to user from a given HTML file.
-func AddBookmarkFile(b bookmarks.Service, log logs.Logger) http.HandlerFunc {
+// AddBookmarksFile attempts to add bookmarks to user from a given HTML file.
+func AddBookmarksFile(b bookmarks.Service, log logs.Logger) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		log.Info("ADD BOOKMARK FILE endpoint hit")
+		vars := mux.Vars(r)
+		APIKey := vars["APIKey"]
 		err := r.ParseMultipartForm(200_000)
 		if err != nil {
 			log.Errorf("Could not parse multipart form: %v", err)
 			w.WriteHeader(404)
 			return
 		}
-		num, err := b.AddBookmarksFromFile(r.Context(), r)
-		if err != nil {
+		num, apierr := b.AddBookmarksFromFile(r.Context(), r, APIKey)
+		if apierr != nil {
+			log.Errorf("Could not add bookmarks from file: %v", apierr)
+			errors.APIErrorResponse(w, apierr)
 			return
 		}
 		log.Info(num)
-		return
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		res := struct{ NumAdded int }{
+			NumAdded: num,
+		}
+		json.NewEncoder(w).Encode(res)
 	}
 }
