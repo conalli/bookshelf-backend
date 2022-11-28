2be7bae3f6084caf7441377254ff9f81691181c8
Conall <conallimlah@gmail.com>
2022-11-28 21:35:32 +0900

    update redirect and add oauth request handlers

--- pkg/http/rest/handlers/oauthredirect.go
+++ pkg/http/rest/handlers/oauthredirect.go
@@ -3,6 +3,7 @@ package handlers
 import (
 	"net/http"
 
+	"github.com/conalli/bookshelf-backend/pkg/errors"
 	"github.com/conalli/bookshelf-backend/pkg/logs"
 	"github.com/conalli/bookshelf-backend/pkg/services/auth"
 )
@@ -12,9 +13,23 @@ func OAuthRedirect(a auth.Service, log logs.Logger) func(w http.ResponseWriter, 
 		err := r.ParseForm()
 		if err != nil {
 			log.Error(err)
+			errors.APIErrorResponse(w, errors.NewInternalServerError())
+			return
 		}
 		log.Infof("%+v", r.Form)
-		a.OAuthFlow(r.Context(), r.FormValue("code"))
+		stateCookie, err := r.Cookie("state")
+		if err != nil {
+			log.Error(err)
+			errors.APIErrorResponse(w, errors.NewInternalServerError())
+			return
+		}
+		nonceCookie, err := r.Cookie("nonce")
+		if err != nil {
+			log.Error(err)
+			errors.APIErrorResponse(w, errors.NewInternalServerError())
+			return
+		}
+		a.OAuthRedirect(r.Context(), r.FormValue("code"), r.FormValue("state"), stateCookie, nonceCookie)
 		w.WriteHeader(http.StatusOK)
 	}
 }
---
+++ pkg/http/rest/handlers/oauthrequest.go
@@ -0,0 +1,40 @@
+package handlers
+
+import (
+	"net/http"
+	"time"
+
+	"github.com/conalli/bookshelf-backend/pkg/errors"
+	"github.com/conalli/bookshelf-backend/pkg/logs"
+	"github.com/conalli/bookshelf-backend/pkg/services/auth"
+)
+
+func OAuthRequest(a auth.Service, log logs.Logger) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		data, err := a.OAuthRequest(r.Context())
+		if err != nil {
+			log.Error(err)
+			errors.APIErrorResponse(w, errors.NewInternalServerError())
+			return
+		}
+		stateCookie := &http.Cookie{
+			Name:     "state",
+			Value:    data.State,
+			MaxAge:   120,
+			HttpOnly: true,
+			Path:     "/api",
+			Expires:  time.Now().Add(2 * time.Minute),
+		}
+		nonceCookie := &http.Cookie{
+			Name:     "nonce",
+			Value:    data.Nonce,
+			MaxAge:   120,
+			HttpOnly: true,
+			Path:     "/api",
+			Expires:  time.Now().Add(2 * time.Minute),
+		}
+		http.SetCookie(w, stateCookie)
+		http.SetCookie(w, nonceCookie)
+		http.Redirect(w, r, data.AuthURL, http.StatusFound)
+	}
+}
