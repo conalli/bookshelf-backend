5eb8a7e702064f67d14f0bf267befac8ca39b9a5
Conall <conallimlah@gmail.com>
2022-11-28 21:36:22 +0900

    add basic token exchange logic

--- pkg/services/auth/service.go
+++ pkg/services/auth/service.go
@@ -3,46 +3,81 @@ package auth
 import (
 	"context"
 	"encoding/json"
-	"fmt"
 	"net/http"
-	"os"
 
+	"github.com/conalli/bookshelf-backend/pkg/errors"
 	"github.com/conalli/bookshelf-backend/pkg/logs"
+	"github.com/coreos/go-oidc/v3/oidc"
 	"github.com/go-playground/validator/v10"
 	"golang.org/x/oauth2"
-	"golang.org/x/oauth2/endpoints"
 )
 
 type Service interface {
-	OAuthFlow(ctx context.Context, code string) (string, error)
+	OAuthRequest(ctx context.Context) (OAuth2Request, error)
+	OAuthRedirect(ctx context.Context, code, state string, stateCookie, nonceCookie *http.Cookie) (string, error)
 }
 
 type service struct {
 	l logs.Logger
 	v *validator.Validate
+	p *oidc.Provider
 }
 
-func NewService(l logs.Logger, v *validator.Validate) *service {
-	return &service{l, v}
+func NewService(l logs.Logger, v *validator.Validate, p *oidc.Provider) *service {
+	return &service{l, v, p}
 }
 
-func (s *service) OAuthFlow(ctx context.Context, code string) (string, error) {
-	url := fmt.Sprintf("%s?client_id=%s&client_secret=%s&code=%s&grant_type=authorization_code&redirect_uri=http://localhost:8080/api/oauth/redirect", endpoints.Google.TokenURL, os.Getenv("GOOGLE_CLIENT_ID"), os.Getenv("GOOGLE_CLIENT_SECRET"), code)
-	req, err := http.NewRequestWithContext(ctx, http.MethodPost, url, nil)
+func (s *service) OAuthRequest(ctx context.Context) (OAuth2Request, error) {
+	state := ""
+	nonce := ""
+	url := googleOAuth2Config.AuthCodeURL(state, oidc.Nonce(nonce), oauth2.AccessTypeOffline)
+	return OAuth2Request{State: state, Nonce: nonce, AuthURL: url}, nil
+}
+
+func (s *service) OAuthRedirect(ctx context.Context, code, state string, stateCookie, nonceCookie *http.Cookie) (string, error) {
+	if state != stateCookie.Value {
+		s.l.Error("INCORRECT STATE FROM REDIRECT")
+		return "", errors.NewBadRequestError("OMG")
+	}
+	oauth2Token, err := googleOAuth2Config.Exchange(ctx, code)
 	if err != nil {
 		s.l.Error(err)
 		return "", err
 	}
-	req.Header.Set("accept", "application/json")
-	res, err := http.DefaultClient.Do(req)
-	if err != nil {
-		s.l.Error(err)
-		return "", err
+	verifier := s.p.Verifier(oidcConfig)
+	rawIDToken, ok := oauth2Token.Extra("id_token").(string)
+	if !ok {
+		s.l.Error("NO id_token in token")
+		return "", errors.NewInternalServerError()
 	}
-	defer res.Body.Close()
-	var t oauth2.Token
-	json.NewDecoder(res.Body).Decode(&t)
+	IDToken, err := verifier.Verify(ctx, rawIDToken)
+	if err != nil {
+		s.l.Error("Could not verify id_token:", err)
+		return "", errors.NewInternalServerError()
+	}
+	if IDToken.Nonce != nonceCookie.Value {
+		s.l.Error("Nonce did not match", IDToken.Nonce, nonceCookie.Value)
+		return "", errors.NewBadRequestError("nonce did not match")
+	}
+	tokens := struct {
+		OAuth2Token   oauth2.Token
+		IDTokenClaims struct {
+			Email, EmailVerified, FamilyName, GivenName, Locale, Name, Picture string
+		}
+	}{OAuth2Token: *oauth2Token}
 
-	s.l.Infof("%+v", t)
+	if err = IDToken.Claims(&tokens.IDTokenClaims); err != nil {
+		s.l.Error("Could not parse id_token claims", err)
+		return "", errors.NewInternalServerError()
+	}
+	j, err := json.Marshal(tokens)
+	if err != nil {
+		s.l.Error("Error marshaling JSON: ", err)
+		return "", errors.NewInternalServerError()
+	}
+
+	s.l.Infof("IDToken: %+v", tokens)
+	s.l.Infof("JSON: %+v", j)
+
 	return "", nil
 }
