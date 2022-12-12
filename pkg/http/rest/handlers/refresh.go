c023fd4bff309f9e6e68fb7c28c83466d1888753
Conall <conallimlah@gmail.com>
2022-12-13 00:39:33 +0900

    add refresh endpoint

--- pkg/http/rest/handlers/refresh.go
+++ pkg/http/rest/handlers/refresh.go
@@ -12,12 +12,26 @@ import (
 func Refresh(a auth.Service, log logs.Logger) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		APIKey, ok := request.GetAPIKeyFromContext(r)
-		if !ok {
+		if len(APIKey) < 1 || !ok {
 			log.Error("could not get APIKey from context")
 			errors.APIErrorResponse(w, errors.NewInternalServerError())
 			return
 		}
-		log.Info(APIKey)
-
+		code := request.FilterCookies(r.Cookies(), auth.BookshelfTokenCode)
+		if code == nil {
+			log.Error("no code cookie in request")
+			errors.APIErrorResponse(w, errors.NewBadRequestError("incorrect information in request"))
+			return
+		}
+		tokens, apierr := a.RefreshTokens(r.Context(), APIKey, code.Value)
+		if apierr != nil {
+			log.Error("could not refresh tokens")
+			errors.APIErrorResponse(w, apierr)
+			return
+		}
+		cookies := tokens.NewTokenCookies(log)
+		log.Info("successfully returned token as cookie")
+		auth.AddCookiesToResponse(w, cookies)
+		w.WriteHeader(http.StatusOK)
 	}
 }
--- pkg/services/auth/service.go
+++ pkg/services/auth/service.go
@@ -19,13 +19,16 @@ type Repository interface {
 	GetUserByEmail(ctx context.Context, email string) (accounts.User, error)
 	NewUser(context.Context, request.SignUp) (accounts.User, error)
 	NewOAuthUser(context.Context, GoogleIDTokenClaims) (accounts.User, error)
+	NewRefreshToken(ctx context.Context, APIKey, refreshToken string) error
+	GetRefreshTokenByAPIKey(ctx context.Context, APIKey string) (string, error)
 }
 
 type Service interface {
-	SignUp(context.Context, request.SignUp) (accounts.User, errors.APIErr)
-	LogIn(context.Context, request.LogIn) (accounts.User, errors.APIErr)
+	SignUp(context.Context, request.SignUp) (*bookshelfTokens, accounts.User, errors.APIErr)
+	LogIn(context.Context, request.LogIn) (*bookshelfTokens, accounts.User, errors.APIErr)
 	OAuthRequest(ctx context.Context, authProvider, authType string) (OIDCRequest, errors.APIErr)
-	OAuthRedirect(ctx context.Context, authProvider, authType, code, state string, cookies []*http.Cookie) (accounts.User, errors.APIErr)
+	OAuthRedirect(ctx context.Context, authProvider, authType, code, state string, cookies []*http.Cookie) (*bookshelfTokens, accounts.User, errors.APIErr)
+	RefreshTokens(ctx context.Context, APIKey, code string) (*bookshelfTokens, errors.APIErr)
 }
 
 type service struct {
@@ -40,46 +43,66 @@ func NewService(l logs.Logger, v *validator.Validate, p *oidc.Provider, db Repos
 }
 
 // SignUp returns the url of a given cmd.
-func (s *service) SignUp(ctx context.Context, requestData request.SignUp) (accounts.User, errors.APIErr) {
+func (s *service) SignUp(ctx context.Context, requestData request.SignUp) (*bookshelfTokens, accounts.User, errors.APIErr) {
 	reqCtx, cancelFunc := request.CtxWithDefaultTimeout(ctx)
 	defer cancelFunc()
 	validateErr := s.validate.Struct(requestData)
 	if validateErr != nil {
 		s.log.Errorf("Could not validate SIGN UP request: %v", validateErr)
-		return accounts.User{}, errors.NewBadRequestError("request format incorrect.")
+		return nil, accounts.User{}, errors.NewBadRequestError("request format incorrect.")
 	}
 	userExists, err := s.db.UserAlreadyExists(ctx, requestData.Email)
 	if err != nil {
 		s.log.Errorf("error attempting to check if user exists: %v", err)
-		return accounts.User{}, errors.NewInternalServerError()
+		return nil, accounts.User{}, errors.NewInternalServerError()
 	}
 	if userExists {
 		s.log.Errorf("error creating new user; user with email %s already exists", requestData.Email)
-		return accounts.User{}, errors.NewBadRequestError("user already exists")
+		return nil, accounts.User{}, errors.NewBadRequestError("user already exists")
 	}
 	user, err := s.db.NewUser(reqCtx, requestData)
 	if err != nil {
 		s.log.Errorf("couldnt create new user: %v", err)
-		return accounts.User{}, errors.NewInternalServerError()
+		return nil, accounts.User{}, errors.NewInternalServerError()
 	}
-	return user, nil
+	tokens, err := NewTokens(s.log, user.APIKey)
+	if err != nil {
+		s.log.Error("could not create new tokens")
+		return nil, accounts.User{}, errors.NewInternalServerError()
+	}
+	err = s.db.NewRefreshToken(ctx, user.APIKey, tokens.refreshToken)
+	if err != nil {
+		s.log.Error("could not save refresh token to db")
+		return nil, accounts.User{}, errors.NewInternalServerError()
+	}
+	return tokens, user, nil
 }
 
 // Login takes in request data, checks the db and returns the username and apikey is successful.
-func (s *service) LogIn(ctx context.Context, requestData request.LogIn) (accounts.User, errors.APIErr) {
+func (s *service) LogIn(ctx context.Context, requestData request.LogIn) (*bookshelfTokens, accounts.User, errors.APIErr) {
 	reqCtx, cancelFunc := request.CtxWithDefaultTimeout(ctx)
 	defer cancelFunc()
 	validateErr := s.validate.Struct(requestData)
 	if validateErr != nil {
 		s.log.Errorf("Could not validate LOG IN request: %v", validateErr)
-		return accounts.User{}, errors.NewBadRequestError("request format incorrect.")
+		return nil, accounts.User{}, errors.NewBadRequestError("request format incorrect.")
 	}
 	user, err := s.db.GetUserByEmail(reqCtx, requestData.Email)
 	if err != nil || !CheckHashedPassword(user.Password, requestData.Password) {
 		s.log.Errorf("could not login: %+v", err)
-		return accounts.User{}, errors.NewAPIError(http.StatusUnauthorized, errors.ErrWrongCredentials.Error(), "error: name or password incorrect")
+		return nil, accounts.User{}, errors.NewAPIError(http.StatusUnauthorized, errors.ErrWrongCredentials.Error(), "error: name or password incorrect")
 	}
-	return user, nil
+	tokens, err := NewTokens(s.log, user.APIKey)
+	if err != nil {
+		s.log.Error("could not create new tokens")
+		return nil, accounts.User{}, errors.NewInternalServerError()
+	}
+	err = s.db.NewRefreshToken(ctx, user.APIKey, tokens.refreshToken)
+	if err != nil {
+		s.log.Error("could not save refresh token to db")
+		return nil, accounts.User{}, errors.NewInternalServerError()
+	}
+	return tokens, user, nil
 }
 
 func (s *service) OAuthRequest(ctx context.Context, authProvider, authType string) (OIDCRequest, errors.APIErr) {
@@ -98,26 +121,36 @@ func (s *service) OAuthRequest(ctx context.Context, authProvider, authType strin
 	return OIDCRequest{State: state, Nonce: nonce, AuthURL: url}, nil
 }
 
-func (s *service) OAuthRedirect(ctx context.Context, authProvider, authType, code, state string, cookies []*http.Cookie) (accounts.User, errors.APIErr) {
+func (s *service) OAuthRedirect(ctx context.Context, authProvider, authType, code, state string, cookies []*http.Cookie) (*bookshelfTokens, accounts.User, errors.APIErr) {
 	stateCookie := request.FilterCookies(cookies, state)
 	if stateCookie == nil {
 		s.log.Error("no cookies in request")
-		return accounts.User{}, errors.NewBadRequestError("no cookies in auth request")
+		return nil, accounts.User{}, errors.NewBadRequestError("no cookies in auth request")
 	}
 	if state != stateCookie.Value {
 		s.log.Error("state values did not match: %s - %s")
-		return accounts.User{}, errors.NewBadRequestError("invalid token")
+		return nil, accounts.User{}, errors.NewBadRequestError("invalid token")
 	}
 	switch authProvider {
 	case "google":
-		user, err := s.GoogleOAuthRedirect(ctx, authType, code, cookies)
-		if err != nil {
-			return accounts.User{}, err
+		user, apierr := s.GoogleOAuthRedirect(ctx, authType, code, cookies)
+		if apierr != nil {
+			return nil, accounts.User{}, apierr
 		}
-		return user, nil
+		tokens, err := NewTokens(s.log, user.APIKey)
+		if err != nil {
+			s.log.Error("could not create new tokens")
+			return nil, accounts.User{}, errors.NewInternalServerError()
+		}
+		err = s.db.NewRefreshToken(ctx, user.APIKey, tokens.refreshToken)
+		if err != nil {
+			s.log.Error("could not save refresh token to db")
+			return nil, accounts.User{}, errors.NewInternalServerError()
+		}
+		return tokens, user, nil
 	default:
 		s.log.Error("invalid auth provider in request url")
-		return accounts.User{}, errors.NewBadRequestError("invalid auth provider in request url")
+		return nil, accounts.User{}, errors.NewBadRequestError("invalid auth provider in request url")
 	}
 }
 
@@ -143,16 +176,15 @@ func (s *service) GoogleOAuthRedirect(ctx context.Context, authType, code string
 		s.log.Errorf("nonces did not match: %s - %s", IDToken.Nonce, nonceCookie.Value)
 		return accounts.User{}, errors.NewBadRequestError("invalid token")
 	}
-	tokens := &GoogleOIDCTokens{OAuth2Token: *oauth2Token}
-	if err = IDToken.Claims(&tokens.IDTokenClaims); err != nil {
+	oidcTokens := &GoogleOIDCTokens{OAuth2Token: *oauth2Token}
+	if err = IDToken.Claims(&oidcTokens.IDTokenClaims); err != nil {
 		s.log.Error("could not parse id_token claims", err)
 		return accounts.User{}, errors.NewInternalServerError()
 	}
-	s.log.Infof("Tokens: %+v", tokens)
 	var user accounts.User
 	if authType == "login" {
 		s.log.Info("login request")
-		userInfo, err := s.db.GetUserByEmail(ctx, tokens.IDTokenClaims.Email)
+		userInfo, err := s.db.GetUserByEmail(ctx, oidcTokens.IDTokenClaims.Email)
 		if err != nil {
 			s.log.Error(err)
 			return accounts.User{}, errors.NewBadRequestError("couldnt find user with given email")
@@ -160,16 +192,16 @@ func (s *service) GoogleOAuthRedirect(ctx context.Context, authType, code string
 		user = userInfo
 	} else {
 		s.log.Info("signup request")
-		userExists, err := s.db.UserAlreadyExists(ctx, tokens.IDTokenClaims.Email)
+		userExists, err := s.db.UserAlreadyExists(ctx, oidcTokens.IDTokenClaims.Email)
 		if err != nil {
 			s.log.Errorf("error attempting to check if user exists: %v", err)
 			return accounts.User{}, errors.NewInternalServerError()
 		}
 		if userExists {
-			s.log.Errorf("error creating new user; user with email %s already exists", tokens.IDTokenClaims.Email)
+			s.log.Errorf("error creating new user; user with email %s already exists", oidcTokens.IDTokenClaims.Email)
 			return accounts.User{}, errors.NewBadRequestError("user already exists")
 		}
-		userInfo, err := s.db.NewOAuthUser(ctx, tokens.IDTokenClaims)
+		userInfo, err := s.db.NewOAuthUser(ctx, oidcTokens.IDTokenClaims)
 		if err != nil {
 			s.log.Errorf("couldnt create user from id token: %v", err)
 			return accounts.User{}, errors.NewInternalServerError()
@@ -177,4 +209,31 @@ func (s *service) GoogleOAuthRedirect(ctx context.Context, authType, code string
 		user = userInfo
 	}
 	return user, nil
+}
+
+func (s *service) RefreshTokens(ctx context.Context, APIKey, code string) (*bookshelfTokens, errors.APIErr) {
+	token, err := s.db.GetRefreshTokenByAPIKey(ctx, APIKey)
+	if err != nil {
+		s.log.Error("could not get refresh token from db")
+		if err == errors.ErrInternalServerError {
+			return nil, errors.NewInternalServerError()
+		}
+		return nil, errors.NewAPIError(http.StatusNotFound, err.Error(), "no refresh token")
+	}
+	_, err = ParseJWT(s.log, token, code)
+	if err != nil {
+		s.log.Error("parsed refresh token invalid")
+		return nil, errors.NewBadRequestError("invalid refresh token")
+	}
+	tokens, err := NewTokens(s.log, APIKey)
+	if err != nil {
+		s.log.Error("could not create new tokens")
+		return nil, errors.NewInternalServerError()
+	}
+	err = s.db.NewRefreshToken(ctx, APIKey, tokens.refreshToken)
+	if err != nil {
+		s.log.Error("could not save refresh token to db")
+		return nil, errors.NewInternalServerError()
+	}
+	return tokens, nil
 }
